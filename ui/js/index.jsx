415c983d7db8eb1356537e0f4ac90f0f89a533ed
yamadapc <tacla.yamada@gmail.com>
2015-06-12 14:13:46 -0300

    Update UI code (fixes problems/moves forward)

--- ui/js/index.jsx
+++ ui/js/index.jsx
@@ -1,6 +1,30 @@
 'use strict';
-var React = require('react/addons');
-var OlhoVivoMap = require('./olhovivo-map');
+var _ = require('lodash');
+var io = require('socket.io-client')('localhost:4000');
+var markers = [];
+
+io.on('positions', function(positions) {
+  markers.push(positions.map(function(position) {
+    return new google.maps.LatLng(position.py, position.px);
+  }));
+});
 
 var renderSite = document.getElementById('app');
-React.render(<OlhoVivoMap googleMapsApi={google.maps} />, renderSite);
+var map = new google.maps.Map(renderSite, {
+  center: new google.maps.LatLng(-23.5435343, -46.6179336),
+  zoom: 12,
+  mapTypeId: google.maps.MapTypeId.ROADMAP,
+});
+
+// var heatmap;
+setInterval(function() {
+  console.log(_.flatten(markers).length);
+  if(_.flatten(markers).length) {
+    var heatmap = new google.maps.visualization.HeatmapLayer({
+      data: _.flatten(markers),
+    });
+    heatmap.setMap(map);
+  }
+  markers = [];
+}, 3000);
+
--- ui/js/olhovivo-map.jsx
+++ ui/js/olhovivo-map.jsx
@@ -1,7 +1,7 @@
 'use strict';
 var React = require('react/addons');
 var ReactGoogleMaps = require('react-google-maps');
-var io = require('socket.io-client')('http://localhost:9000');
+var io = require('socket.io-client')('http://localhost:4000');
 
 var GoogleMapsMixin = ReactGoogleMaps.GoogleMapsMixin;
 var Map = ReactGoogleMaps.Map;
@@ -11,44 +11,32 @@ var OlhoVivoMap = React.createClass({
   displayName: "OlhoVivoMap",
   mixins: [GoogleMapsMixin],
 
-  getInitialState: function() {
-    return { markers: [] };
-  },
-
-  componentDidMount: function() {
-    var _this = this;
-    io.on('positions', function(positions) {
-      var markers = positions.map(function(position) {
-        return {
-          position: {
-            lat: position.py,
-            lng: position.px,
-          },
-          key: position.p,
-        }
-      });
-      console.log(positions);
-      console.log(markers);
-
-      _this.setState({ markers: markers, });
-    });
-  },
-
-        // <ToastContainer ref="toast" toastMessageFactory={React.createFactory(ToastMessage.jQuery)}/>
   render: function() {
     var center = new google.maps.LatLng(-23.5435343, -46.6179336);
 
     return (
       <div style={{height: "100%"}}>
         <Map ref="map" style={ { height: "100%", } } zoom={12} center={center} />
-        {this.state.markers.map(toMarker, this)}
+        <OlhoVivoHeatmap />
       </div>
     );
-
-    function toMarker(marker, index) {
-      return <Marker position={marker.position} key={marker.key} />;
-    }
   }
+});
+
+var OlhoVivoHeatmap = React.createClass({
+  getInitialState: function() {
+    return { positions: [] };
+  },
+
+  componentDidMount: function() {
+    var _this = this;
+    var markers = [];
+
+  },
+
+  render: function() {
+    return <div></div>
+  },
 });
 
 exports = module.exports = OlhoVivoMap;
