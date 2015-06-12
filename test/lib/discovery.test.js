3faadc9822ce702a23b0bea492ac89d115d68e31
yamadapc <tacla.yamada@gmail.com>
2015-06-12 14:15:41 -0300

    Add drafts for dicovery module's tests

---
+++ test/lib/discovery.test.js
@@ -0,0 +1,37 @@
+'use strict'; /* global describe, it, before, beforeEach */
+var makeStub = require('mocha-make-stub');
+var should = require('should');
+var OlhoVivoApi = require('../../lib/index.js');
+
+var discovery = require('../../lib/discovery');
+
+describe('discovery', function() {
+  before(function() {
+    this.olhovivoApi = new OlhoVivoApi({
+      token: process.env.SPTRANS_TOKEN,
+    });
+  });
+
+  describe('.RouteStopsStream(options)', function() {
+    it('gets exposed', function() {
+      should.exist(discovery.RouteStopsStream);
+      discovery.RouteStopsStream.should.be.instanceof(Function);
+    });
+
+    beforeEach(function() {
+      this.routeStopsStream = new discovery.RouteStopsStream({
+        olhovivoApi: this.olhovivoApi,
+      });
+    });
+
+    describe('._processRoute(route_id)', function() {
+      makeStub('routeStopsStream', 'push');
+
+      it('discover stops for a certain route', function() {
+        this.routeStopsStream._processRoute(discovery.routes[0])
+          .then(function() {
+          });
+      });
+    });
+  });
+});
